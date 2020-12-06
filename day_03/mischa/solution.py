e92f329d7831402146741df98a653e85ba31753d
voidlessVoid <58170899+voidlessVoid@users.noreply.github.com>
2020-12-06 13:42:40 +0200

    add_day3

--- day_03/mischa/solution.py
+++ day_03/mischa/solution.py
@@ -22,9 +22,26 @@ def read_input_text():
     with open('input.txt', 'r') as fh:
         return fh.read().strip()
 
+data = read_input_text()
+def part_a(inp):
+    dict_of_pos = defaultdict(list)
+    pos=[0,0] #x,y
+    for idc,c in enumerate(inp):
+        dict_of_pos[str(pos)].append(idc)
+        if c =='^':
+            pos[1]+=1
+        elif c =='v':
+            pos[1]-=1
+        elif c =='>':
+            pos[0]+=1
+        elif c =='<':
+            pos[0]-=1
+        #print(c,pos)
+    a = [x for x in dict_of_pos if len(dict_of_pos[x])>0]
+    print(len(a))
 
-def part_a():
-    pass
+part_a(data)
+#2081
 
 def part_b():
     pass
